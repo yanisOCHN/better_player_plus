ffdf0a77909172c918fd2abc90261953b2dfc6e6
Yanis Ouchène <yanis.ouchene@theodo.com>
2026-07-01 15:26:40 +0200

    feat: expose BetterPlayerController in BetterPlayerListVideoPlayerController

--- lib/src/list/better_player_list_video_player_controller.dart
+++ lib/src/list/better_player_list_video_player_controller.dart
@@ -1,32 +1,33 @@
 import 'package:better_player_plus/better_player_plus.dart';
+import 'package:better_player_plus/src/video_player/video_player.dart';
 
 ///Controller of Better Player List Video Player.
 class BetterPlayerListVideoPlayerController {
-  BetterPlayerController? _betterPlayerController;
+  BetterPlayerController? innerBetterPlayerController;
 
   void setVolume(double volume) {
-    _betterPlayerController?.setVolume(volume);
+    innerBetterPlayerController?.setVolume(volume);
   }
 
   void pause() {
-    _betterPlayerController?.pause();
+    innerBetterPlayerController?.pause();
   }
 
   void play() {
-    _betterPlayerController?.play();
+    innerBetterPlayerController?.play();
   }
 
   void seekTo(Duration duration) {
-    _betterPlayerController?.seekTo(duration);
+    innerBetterPlayerController?.seekTo(duration);
   }
 
   // Kept as a method (not a setter) to maintain a consistent API with the rest of the controller
   // ignore: use_setters_to_change_properties
   void setBetterPlayerController(BetterPlayerController? betterPlayerController) {
-    _betterPlayerController = betterPlayerController;
+    innerBetterPlayerController = betterPlayerController;
   }
 
   void setMixWithOthers(bool mixWithOthers) {
-    _betterPlayerController?.setMixWithOthers(mixWithOthers);
+    innerBetterPlayerController?.setMixWithOthers(mixWithOthers);
   }
 }
